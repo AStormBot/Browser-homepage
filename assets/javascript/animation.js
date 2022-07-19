f48dcc5af4fd163c46e1a1ec6dcb847f257437cd
AStorm <80943478+AStormBot@users.noreply.github.com>
2022-07-19 19:13:42 +0430

    setting added

---
+++ assets/javascript/animation.js
@@ -0,0 +1,18 @@
+let name12 = document.getElementById("Header-name");
+const hour = new Date().getHours();
+const name = localStorage.getItem('user-name') || "User";
+const animation_check = localStorage.getItem('user-animation') || "come-in-text";
+let random;
+if (hour > 6 && hour < 12) {
+    random = headerNames.morning;
+} else if (hour < 18) {
+    random = headerNames.afternoon;
+} else if (hour < 21) {
+    random = headerNames.evening;
+} else random = headerNames.night;
+
+let text = random[Math.floor(Math.random() * random.length)] + ", " + name;
+
+for (let i = 0; i < text.length; i++) {
+    name12.innerHTML += `<pre class="${animation_check} float-start" style="animation-delay: ${i * 50}ms">${text[i]}</pre>`;
+}
