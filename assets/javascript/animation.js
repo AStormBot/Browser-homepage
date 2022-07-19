8b8d48f9e4ad9744244f0038fd5dd852b286396b
NikanWasTaken <nikan1252sabet4801@gmail.com>
2022-07-19 20:19:24 +0430

    changed somethings idk

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -1,18 +1,22 @@
 let name12 = document.getElementById("Header-name");
 const hour = new Date().getHours();
-const name = localStorage.getItem('user-name') || "User";
-const animation_check = localStorage.getItem('user-animation') || "come-in-text";
+const name = localStorage.getItem("user-name");
+const animation_check = localStorage.getItem("user-animation") || "typing-animation";
 let random;
+
 if (hour > 6 && hour < 12) {
-    random = headerNames.morning;
+	random = headerNames.morning;
 } else if (hour < 18) {
-    random = headerNames.afternoon;
+	random = headerNames.afternoon;
 } else if (hour < 21) {
-    random = headerNames.evening;
+	random = headerNames.evening;
 } else random = headerNames.night;
 
-let text = random[Math.floor(Math.random() * random.length)] + ", " + name;
+let text = `${random[Math.floor(Math.random() * random.length)]}${name.length ? `, ${name}` : ""}!`;
 
 for (let i = 0; i < text.length; i++) {
-    name12.innerHTML += `<pre class="${animation_check} float-start" style="animation-delay: ${i * 50}ms">${text[i]}</pre>`;
+	name12.innerHTML += `<pre class="${animation_check} float-start" style="animation-delay: ${
+		i * 50
+	}ms" text-align=center>${text[i]}</pre>`;
 }
+
