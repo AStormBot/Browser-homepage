596178c035712b49e4624759af23dfae6031b0db
AStorm <80943478+AStormBot@users.noreply.github.com>
2022-07-19 20:47:59 +0430

    marged

--- assets/javascript/animation.js
+++ assets/javascript/animation.js
@@ -1,8 +1,9 @@
 let name12 = document.getElementById("Header-name");
 const hour = new Date().getHours();
-const name = localStorage.getItem('user-name') || "User";
-const animation_check = localStorage.getItem('user-animation') || "come-in-text";
+const name = localStorage.getItem("user-name");
+const animation_check = localStorage.getItem("user-animation") || "typing-animation";
 let random;
+
 if (hour > 6 && hour < 12) {
     random = headerNames.morning;
 } else if (hour < 18) {
@@ -11,8 +12,11 @@ if (hour > 6 && hour < 12) {
     random = headerNames.evening;
 } else random = headerNames.night;
 
-let text = random[Math.floor(Math.random() * random.length)] + ", " + name;
+let text = `${random[Math.floor(Math.random() * random.length)]}${name.length ? `, ${name}` : ""}!`;
 
 for (let i = 0; i < text.length; i++) {
-    name12.innerHTML += `<pre class="${animation_check} float-start" style="animation-delay: ${i * 50}ms">${text[i]}</pre>`;
+    name12.innerHTML += `<pre class="${animation_check} float-start" style="animation-delay: ${
+        i * 50
+    }ms" text-align=center>${text[i]}</pre>`;
 }
+
--- assets/javascript/setting.js
+++ assets/javascript/setting.js
@@ -1,8 +1,12 @@
 function save_setting() {
     const local = localStorage;
-    const user_name = document.getElementById('floatingInput').value;
-    const user_animation = document.getElementById('animation-type').value;
-    
-    local.setItem('user-name', user_name);
-    local.setItem('user-animation', user_animation)
+    const user_name = document.getElementById("floatingInput").value;
+    const user_animation = document.getElementById("animation-type").value;
+
+    local.setItem("user-name", user_name);
+    local.setItem("user-animation", user_animation);
+
+    for (let i = 0; i < 6; i++) {
+        local.setItem(`${i}`, document.getElementById(`${i + 1}`).value)
+    }
 }
