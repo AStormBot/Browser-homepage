231998664167879d78a74f2870227f7275df780d
NikanWasTaken <nikanwastaken@users.noreply.github.com>
2022-08-18 20:58:15 +0430

    feat: add working clock

--- assets/javascript/background.js
+++ assets/javascript/background.js
@@ -1,8 +1,8 @@
 if (!localStorage.getItem('0-pic') && !localStorage.getItem('1-pic') && !localStorage.getItem('2-pic')) {
     const body = $('#body');
     const backgrounds = ['1.jpg', '2.png', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.png', '8.jpg', '9.jpg', '10.png', '11.jpg'];
-    body.html(`<img src="./assets/Images/${backgrounds[Math.floor(Math.random() * backgrounds.length)]}" alt="">`);
-}else {
+    body.html(`<img src="./assets/Images/background/${backgrounds[Math.floor(Math.random() * backgrounds.length)]}" alt="">`);
+} else {
     const local = localStorage;
     const body = $('#body');
     const backgrounds = [local.getItem('0-pic'), local.getItem('1-pic'), local.getItem('2-pic')];
@@ -15,5 +15,5 @@ if (!localStorage.getItem('0-pic') && !localStorage.getItem('1-pic') && !localSt
 
     const finally_pic = random_pic.split('\\')[2];
 
-    body.html(`<img src="./assets/Images/${finally_pic}">`)
+    body.html(`<img src="./assets/Images/background/${finally_pic}">`);
 }
